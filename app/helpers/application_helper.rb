d9ad9d2aa78c2c48553641f30a45b7d8d2280881
omagdiel <omagdiel@gmail.com>
2018-02-12 14:15:56 -0700

    Changed tech news nav link location and nav position reactive."

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,16 +35,16 @@ module ApplicationHelper
         title: 'Contact'
       },
       {
+        url: tech_news_path,
+        title: 'Tech News'
+      },
+      {
         url: blogs_path,
         title: 'Blog'
       },
       {
         url: portfolios_path,
         title: 'Portfolio'
-      },
-      {
-        url: tech_news_path,
-        title: 'Tech News'
       }
     ]
   end
