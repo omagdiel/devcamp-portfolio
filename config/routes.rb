b232b9911f715b593ca248eddfc66d377aafe1de
omagdiel <omagdiel@gmail.com>
2018-02-12 11:46:39 -0700

    Implemented Twitter call on tech news page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-  
+
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   get 'about-me', to:'pages#about'
   get 'contact', to:'pages#contact'
+  get 'tech_news', to:'pages#tech_news'
 
   resources :blogs do
     member do
