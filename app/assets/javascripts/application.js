63a771fee1477b5a837fa97a7c26c99f80cc9199
omagdiel <omagdiel@gmail.com>
2018-03-15 11:32:04 -0600

    Fixed ping bug by moving it to bottom of js list.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,8 @@
 //= require popper
 //= require_tree .
 //= require bootstrap-sprockets
+
+var http = require("http");
+setInterval(function() {
+    http.get("https://owen-magdiel-portfolio.herokuapp.com/");
+}, 300000); // every 5 minutes (300000)
